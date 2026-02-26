2227ef20eb3f3584d0e14d0f554ff29bfd2f42f6
Kartik Chouksey <kartik@example.com>
2026-02-27 04:30:53 +0530

    fix: add prisma postinstall and health check for Vercel

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/', (req, res) => {
+    res.send('Bitespeed Identity Reconciliation Service is running. Use POST /identify to interact.');
+});
+
 app.post('/identify', identify);
 
 export default app;
